02a814278fe4549a4ac7eae2887cc794ba7c3133
kellydinneen <kgdinneen@gmail.com>
2020-12-02 09:59:10 -0800

    Import Turn class for Round tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,20 +4,21 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 
 describe('Round', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const round = new Round();
     expect(Round).to.be.a('function');
   });
 
-  it.skip('should be an instance of Round', function() {
+  it('should be an instance of Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it.skip('should have a deck', function() {
+  it('should have a deck', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card3 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
