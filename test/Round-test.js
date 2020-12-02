1519cf76b827648c34f5b5599bc9a5ea33056157
kellydinneen <kgdinneen@gmail.com>
2020-12-02 09:40:33 -0800

    Test for updates to round during play

--- test/Round-test.js
+++ test/Round-test.js
@@ -53,7 +53,7 @@ describe('Round', function() {
     expect(round.turns).to.equal(0);
   });
 
-  it.skip('should start with 0 incorrect guesses', function() {
+  it.skip('should start with no incorrect guesses', function() {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card3 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
@@ -62,7 +62,7 @@ describe('Round', function() {
 
     const round = new Round(deck);
 
-    expect(round.incorrectGuesses).to.equal(0);
+    expect(round.incorrectGuesses).to.equal([]);
   });
 
   it.skip('should update turn count', function() {
@@ -80,5 +80,62 @@ describe('Round', function() {
     expect(round.turns).to.equal(2);
   });
 
+  it.skip('should move on to next card', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card3 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    const deck = new Deck([card1, card2, card3])
+
+    const round = new Round(deck);
+    expect(round.returnCurrentCard()).to.equal(card1);
+    round.takeTurn(`object`);
+    expect(round.returnCurrentCard()).to.equal(card2);
+    round.takeTurn('blue');
+    expect(round.returnCurrentCard()).to.equal(card3);
+  });
+
+  it.skip('should store incorrect guesses', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card3 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    const deck = new Deck([card1, card2, card3])
+
+    const round = new Round(deck);
+    expect(round.incorrectGuesses).to.equal([]);
+    round.takeTurn(`function`);
+    expect(round.incorrectGuesses).to.equal([`function`]);
+    round.takeTurn('green');
+    expect(round.incorrectGuesses).to.equal([`function`, 'green']);
+  });
+
+  it.skip('should provide feedback', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card3 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    const deck = new Deck([card1, card2, card3])
+
+    const round = new Round(deck);
+    var turnOneFeedback = round.takeTurn(`function`);
+    expect(turnOneFeedback).to.equal('incorrect!');
+    var turnTwoFeedback = round.takeTurn('blue');
+    expect(turnTwoFeedback).to.equal('correct!');
+  });
+
+  it.skip('should calculate percentage correct guesses', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card3 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+
+    const deck = new Deck([card1, card2, card3])
+
+    const round = new Round(deck);
+    round.takeTurn(`object`);
+    expect(round.calculatePercentCorrect()).to.equal(100);
+    round.takeTurn('green');
+    expect(round.calculatePercentCorrect()).to.equal(50);
+  });
 
 });
