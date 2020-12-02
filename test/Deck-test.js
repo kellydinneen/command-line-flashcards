f7405be1ae80e538858a908c541503c96e0decb7
kellydinneen <kgdinneen@gmail.com>
2020-12-01 16:19:19 -0800

    Debug Deck class tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,17 +6,17 @@ const Card = require('../src/Card');
 
 describe('Deck', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', function() {
+  it('should be an instance of Deck', function() {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  it.skip('should have cards', function() {
+  it('should have cards', function() {
     const card1 = new Card(1, 'Can this deck count its cards?', ['yes', 'no'], 'yes');
     const card2 = new Card(3, 'What does a deck instantiation take as an argument', ['object', 'string', 'array'], 'array');
 
@@ -24,7 +24,7 @@ describe('Deck', function() {
     expect(deck.cards).to.deep.equal([card1, card2]);
   });
 
-  it.skip('should count cards', function() {
+  it('should count cards', function() {
     const card1 = new Card(1, 'Can this deck count its cards?', ['yes', 'no'], 'yes');
     const card2 = new Card(3, 'What does a deck instantiation take as an argument', ['object', 'string', 'array'], 'array');
 
@@ -32,4 +32,4 @@ describe('Deck', function() {
     expect(deck.countCards()).to.equal(2);
   });
 
-};
+});
