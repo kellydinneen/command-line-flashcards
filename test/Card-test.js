393a3dcde726fa75ba088c90974008e049584e8a
kellydinneen <kgdinneen@gmail.com>
2020-12-03 13:46:57 -0700

    Add more tests for Card class

--- test/Card-test.js
+++ test/Card-test.js
@@ -20,13 +20,28 @@ describe('Card', function() {
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });
 
+  it('should be able to store a different question', function() {
+    const card = new Card(1, 'What is a dog?', ['reptile', 'bird', 'mammal'], 'mammal');
+    expect(card.question).to.equal('What is a dog?');
+  });
+
   it('should store a list of possible answers', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });
 
+  it('should be able to store a different list of possible answers', function() {
+    const card = new Card(1, 'What is a dog?', ['reptile', 'bird', 'mammal'], 'mammal');
+    expect(card.answers).to.deep.equal(['reptile', 'bird', 'mammal']);
+  });
+
   it('should store the correct answer', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
+  });
+
+  it('should be able to store a different correct answer', function() {
+    const card = new Card(1, 'What is a dog?', ['reptile', 'bird', 'mammal'], 'mammal');
+    expect(card.correctAnswer).to.equal('mammal');
   });
 });
