62f9260541b2c8e488393a03d28db8cb6f8fdb56
kellydinneen <kgdinneen@gmail.com>
2020-12-03 14:57:14 -0700

    Refactor Game class for testing convenience

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,9 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
 
 class Game {
   constructor() {}
@@ -15,15 +18,24 @@ class Game {
   }
 
   start() {
-    const makeDeck = (deck, questionData) => {
-      let card = new Card(questionData.id, questionData.question, questionData.answers, questionData.correctAnswer);
-      deck.push(card);
-    }
-    const deck = new Deck(prototypeQuestions.reduce(reduction, []);
-    const round = new Round(deck);
+    const deck = this.startDeck();
+    const round = this.startRound(deck);
     this.printMessage(deck, round);
     this.printQuestion(round);
   }
-}
+
+  startDeck() {
+    const newDeck = (deck, questionData) => {
+    let card = new Card(questionData.id, questionData.question, questionData.answers, questionData.correctAnswer);
+    deck.push(card);
+    }
+    return new Deck(prototypeQuestions.reduce(reduction, []));
+  }
+
+  startRound(deck) {
+    return new Round(deck);
+  }
+};
+
 
 module.exports = Game;
