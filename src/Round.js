c2edc9030ce4e57b4048c99cd339b0e91bf600e0
kellydinneen <kgdinneen@gmail.com>
2020-12-03 18:32:03 -0700

    Add method for reviewing incorrect answers

--- src/Round.js
+++ src/Round.js
@@ -5,6 +5,7 @@ class Round {
     this.deck = deck;
     this.turns = 0;
     this.incorrectGuesses = [];
+    this.attemptCount = 1;
   }
   returnCurrentCard() {
     return this.deck.cards[0];
@@ -12,16 +13,25 @@ class Round {
   calculatePercentCorrect() {
     return ((this.turns - this.incorrectGuesses.length) / this.turns) * 100;
   }
+  reviewIncorrectGuesses() {
+    console.log(`*********THAT's ALL THE QUESTIONS!********
+    You answered ${this.calculatePercentCorrect()}% of them correctly. Now, let's review only those that you answered incorrectly. So in this round, you'll answer ${this.incorrectGuesses.length} questions.
+    ---------------------------------------------------`)
+    this.deck.cards = this.incorrectGuesses;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+    this.attemptCount++;
+  }
   endRound() {
     console.log(
-      `**Round over!**
-      You answered ${this.calculatePercentCorrect()}%
-      of the questions correctly!`);
+      `*********YOU'RE DONE!*********
+      It took you ${this.attemptCount} attempts
+      to answer all of the questions correctly!`);
   }
   takeTurn(guess) {
     const turn = new Turn(guess, this.returnCurrentCard());
     if (!turn.evaluateGuess()) {
-      this.incorrectGuesses.push(guess);
+      this.incorrectGuesses.push(this.returnCurrentCard());
     }
     this.turns++;
     this.deck.cards.shift();
