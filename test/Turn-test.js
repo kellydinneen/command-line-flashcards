6e9b08791f386910342521de916cb3a351311d28
kellydinneen <kgdinneen@gmail.com>
2020-12-03 19:56:45 -0700

    Refactor tests for Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,78 +6,44 @@ const Card = require('../src/Card');
 
 describe('Turn', function() {
 
+  const card = new Card(
+    1,
+    'What does this turn take as its first argument',
+    ['guess', 'game', 'ice cream'],
+    'guess'
+  );
+  let turn = new Turn('guess', card);
+
   it('should be a function', function() {
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', function() {
-    const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
   it('should return guess', function() {
-    const card = new Card(
-      1,
-      'What does this turn take as its first argument',
-      ['guess', 'game', 'ice cream'],
-      'guess'
-    );
-    const turn = new Turn('guess', card);
     expect(turn.returnGuess()).to.equal('guess');
   });
 
   it('should return card', function() {
-    const card = new Card(
-      1,
-      'What does this turn take as its first argument',
-      ['guess', 'game', 'ice cream'],
-      'guess'
-    );
-    const turn = new Turn('guess', card);
     expect(turn.returnCard()).to.equal(card);
   });
 
   it('should evaluate guess as true when correct', function() {
-    const card = new Card(
-      1,
-      'What does this turn take as its first argument',
-      ['guess', 'game', 'ice cream'],
-      'guess'
-    );
-    const turn = new Turn('guess', card);
     expect(turn.evaluateGuess()).to.equal(true);
   });
 
-  it('should evaluate guess as false when incorrect', function() {
-    const card = new Card(
-      1,
-      'What does this turn take as its first argument',
-      ['guess', 'game', 'ice cream'],
-      'guess'
-    );
-    const turn = new Turn('ice cream', card);
-    expect(turn.evaluateGuess()).to.equal(false);
-  });
-
   it('should provide positive feedback when guess is correct', function() {
-    const card = new Card(
-      1,
-      'What does this turn take as its first argument',
-      ['guess', 'game', 'ice cream'],
-      'guess'
-    );
-    const turn = new Turn('guess', card);
     expect(turn.giveFeedback()).to.equal('correct!');
   });
 
+  it('should evaluate guess as false when incorrect', function() {
+    turn = new Turn('ice cream', card);
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
+
   it('should provide negative feedback when guess is incorrect', function() {
-    const card = new Card(
-      1,
-      'What does this turn take as its first argument',
-      ['guess', 'game', 'ice cream'],
-      'guess'
-    );
-    const turn = new Turn('ice cream', card);
     expect(turn.giveFeedback()).to.equal('incorrect!');
   });
 });
